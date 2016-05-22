0466c45be0f959a67bd692dccfba671285e12914
cengizzhan <cengiz.han@hotmail.co.uk>
2016-05-22 21:58:28 +0300

    Update TipController.php

--- controllers/TipController.php
+++ controllers/TipController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace backend\modules\yorum\controllers;
+namespace cengizzhan\yorum\controllers;
 
 use Yii;
-use backend\modules\yorum\models\Tip;
-use backend\modules\yorum\models\Yorum;
-use backend\modules\yorum\models\TipSearch;
+use cengizzhan\yorum\models\Tip;
+use cengizzhan\yorum\models\Yorum;
+use cengizzhan\yorum\models\TipSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
