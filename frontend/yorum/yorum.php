74a820c22131919059692a78cfdd29d33e438bb8
cengizzhan <cengiz.han@hotmail.co.uk>
2016-05-24 01:56:51 +0300

    Update yorum.php

--- frontend/yorum/yorum.php
+++ frontend/yorum/yorum.php
@@ -1,14 +1,25 @@
 <?php
 /* @var $this yii\web\View */
-
+use yii\db\Query;
 echo \yii\widgets\ListView::widget([
 'dataProvider'=>$provider,
 'itemView'=>function($model)
 {
+ $query = new Query;
+ $kullanici_adi=$model->yazan;
+ $query ->select('username')
+   ->from('user')
+   ->where (['id' =>$kullanici_adi]);
+
+ $command = $query->createCommand();
+ $kullanici = $command->queryOne();
+ 
 	return '<div class="list-group">
   <a href="#" class="list-group-item active">
-    <h4 class="list-group-item-heading">'.$model->icerik.'</h4>
-    <p class="list-group-item-text">'.$model->baslik.'</p>
+    <h4 class="list-group-item-heading"> Yazar : '.$kullanici['username'].'</h4>
+    <p class="list-group-item-text">Baslık : '.$model->baslik.'</p>
+	 <p class="list-group-item-text">'.$model->icerik.'</p>
+	
   </a>
 </div>';
 }
